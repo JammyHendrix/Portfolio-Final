627a5a8afbaea3e4818c4b28ba4de37fd9a43ada
C Jamison <cjamison@Cs-MacBook-Pro.local>
2018-01-29 11:50:11 -0700

    portfolio

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
-var http = require("http");
-// Define a port to listen for incoming requests
-var PORT = 8080;
-// Create a generic function to handle requests and responses
-function handleRequest(request, response) {
-  // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
-}
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+var express = require("express");
+var bodyParser = require("body-parser");
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+// Tells node that we are creating an "express" server
+var app = express();
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });

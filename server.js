be8d99f3258f204e93ee96398d36f27142ef1cdd
C Jamison <cjamison@Cs-MacBook-Pro.local>
2018-01-29 12:07:23 -0700

    portfolio

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+require("./routes/htmlRoutes")(app);
+
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });

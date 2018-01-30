4febfd7f7f9af4e683ce49d2130f7a122a898751
C Jamison <cjamison@Cs-MacBook-Pro.local>
2018-01-30 16:06:12 -0700

    portfolio

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var path = require('path');
 // ==============================================================================
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -11,9 +12,9 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
-});
